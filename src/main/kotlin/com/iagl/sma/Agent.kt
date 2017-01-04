58822084c5c7dbbc65a1173625690673a0bbd4b8
bacquet <nathan.bacquet@etudiant.univ-lille1.fr>
2017-01-04 17:40:01 +0100

    init commit

---
+++ src/main/kotlin/com/iagl/sma/Agent.kt
@@ -0,0 +1,4 @@
+package com.iagl.sma
+
+open abstract class Agent(var pos : Pair<Int,Int>){
+}
---
+++ src/test/kotlin/com/iagl/sma/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.iagl.sma
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
