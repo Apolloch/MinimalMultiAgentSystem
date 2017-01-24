b4ab056f4beb01e207a9f4e81932e7ea0285c17d
bacquet <nathan.bacquet@etudiant.univ-lille1.fr>
2017-01-24 16:01:16 +0100

    speed ok

--- src/main/kotlin/com/iagl/sma/avatar/Avatar.kt
+++ src/main/kotlin/com/iagl/sma/avatar/Avatar.kt
@@ -8,43 +8,49 @@ import java.awt.Color
  * Created by Apolloch on 20/01/2017.
  */
 
-class Avatar(x :Int , y : Int):Agent(x,y, Color.BLUE) {
+class Avatar(x :Int , y : Int, val dValues : Array<Array<Int>>):Agent(x,y, Color.BLUE) {
+    var age : Int
     var direction: Direction
     private var  tempY: Int
     private var tempX:Int
 
+    private var speed: Int
+
     init {
+        speed = PropertiesAvatar.INSTANCE.speedAvatar
+        age = 0
         tempX = x
         tempY = y
         direction = Direction.IDLE
     }
 
     override fun decide(environnement: Array<Array<Agent?>>) {
+        age++
+        if(age % speed == 0){
+            tempX = x+direction.x
+            tempY = y+direction.y
 
-
-        tempX = x+direction.x
-        tempY = y+direction.y
-
-        if(tempX >= 0 && tempX<environnement.size && tempY >= 0 && tempY<environnement[0].size){
-            if(environnement[tempX][tempY] == null){
-                environnement[x][y]=null
-                x = tempX
-                y=tempY
-                environnement[tempX][tempY]=this
+            if(tempX >= 0 && tempX<environnement.size && tempY >= 0 && tempY<environnement[0].size){
+                if(environnement[tempX][tempY] == null){
+                    environnement[x][y]=null
+                    x = tempX
+                    y=tempY
+                    environnement[tempX][tempY]=this
+                }
+                else{
+                    particleCollisionGestion(environnement)
+                }
             }
             else{
-                particleCollisionGestion(environnement as Array<Array<Agent?>>)
+                borderGestion(environnement)
             }
-        }
-        else{
-            borderGestion(environnement as Array<Array<Agent?>>)
         }
     }
 
     fun borderGestion(environnement: Array<Array<Agent?>>) {
         if (!PropertiesAvatar.INSTANCE.torique)
-            direction=Direction.IDLE
-        if(PropertiesAvatar.INSTANCE.torique ) {
+            direction = Direction.IDLE
+        if (PropertiesAvatar.INSTANCE.torique) {
             if (tempX < 0) {
                 tempX = environnement.size + direction.x
             } else {
@@ -75,6 +81,16 @@ class Avatar(x :Int , y : Int):Agent(x,y, Color.BLUE) {
     }
 
     fun calculateDValues(environnement: Array<Array<Agent?>>){
-        //TODO
+        var distance = 1
+
+        for( dir in Direction.values() ){
+            if(x+dir.x > 0 && x+dir.x < environnement.size && y+dir.x > 0 && y+dir.x < environnement.size && dir!= Direction.IDLE)
+                if(environnement[x+dir.x][y+dir.y] != null){
+                    dValues[x+dir.x][y+dir.y] = distance
+                }
+                else{
+                    dValues[x+dir.x][y+dir.y] = -1
+                }
+        }
     }
 }
